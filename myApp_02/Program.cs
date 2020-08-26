8736b26bf6d49e99124e99601448f5db1858d17d
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-26 11:56:17 +0300

    gitignore

---
+++ myApp_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace myApp_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Input the Number");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i <= user_input; i++)
+            {
+                if (user_input % i == 0)
+                {
+                  count++;
+                  
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine("This is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine("This is not");
+                }
+                
+            }
+        }
+    }
+}
