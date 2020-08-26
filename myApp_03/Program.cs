340cd2995a71f05aa29743134d4e8073d0d503dc
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-26 13:43:48 +0300

    test

--- myApp_03/Program.cs
+++ myApp_03/Program.cs
@@ -5,8 +5,24 @@ namespace myApp_03
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+         {
+            int a = 0;
+            Console.WriteLine("Enter the number:");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+
+          
+         for (int i = 1; i <= user_input; i++) 
+         {
+            if (user_input % i == 0) {
+               a++;
+            }
+         }
+         if (a == 2) {
+            Console.WriteLine("{0} is a Prime Number.", user_input);
+         } else {
+            Console.WriteLine("Not a Prime Number.");
+         }
+         //Console.ReadLine();
+      }
     }
 }
