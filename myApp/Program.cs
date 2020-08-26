125c97b958eb9567d1799ae6958015271f924ca4
farid555 <mohammad.uzzaman2@centria.fi>
2020-08-26 11:02:45 +0300

    1 is done

---
+++ myApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write the number here:");
+            int user_input = Convert.ToInt32(Console.ReadLine());
+            
+            if (user_input % 2 == 0)
+
+            {
+                Console.WriteLine("Even number.");
+            } 
+             else 
+            {
+                Console.WriteLine("Odd number.");
+       }
+        }
+    }
+}
